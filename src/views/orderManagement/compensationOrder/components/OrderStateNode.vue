b388f15d22e8bf0df7cd4076f7a9d86abcf3c719
shibiao <1508113513@qq.com>
2025-12-15 11:47:55 +0800

    fix:文案调整

--- src/views/orderManagement/compensationOrder/components/OrderStateNode.vue
+++ src/views/orderManagement/compensationOrder/components/OrderStateNode.vue
@@ -39,18 +39,18 @@ const dataPage = reactive({
 
 <template>
   <div class="order-state-container">
-    <el-steps :active="dataPage.activeIndex" finish-status="success" align-center>
+    <el-steps class="order-steps" :active="dataPage.activeIndex" finish-status="success" align-center>
       <el-step v-for="(item, index) in dataPage.nodeList" :key="index" :title="item.flowNodeName"
         :description="item.operationTime">
         <template #title>
           <div>
-            <span>{{ item.flowNodeName }}</span>
+            <span>{{ item.operationTime }}</span>
           </div>
         </template>
         <template #description>
-          <div>
-            <span>{{ item.operationTime }}</span>
-            <span class="ml-4">{{ item.operatorName }}</span>
+          <div class="order-content">
+            <span>{{ item.flowNodeName }}</span>
+            <span class="order-desc">{{ item.operationDescription }}</span>
           </div>
         </template>
       </el-step>
@@ -61,5 +61,59 @@ const dataPage = reactive({
 <style lang="scss" scoped>
 .order-state-container {
   width: 100%;
+}
+
+.order-steps {
+  :deep(.el-steps--horizontal) {
+    align-items: flex-start;
+  }
+
+  :deep(.el-step) {
+    position: relative;
+
+    /* 让标题可以定位到节点上方 */
+    .el-step__main {
+      position: relative;
+    }
+
+    .el-step__title {
+      position: absolute;
+      top: -70px;
+      left: 50%;
+      transform: translateX(-50%);
+      font-size: 14px;
+      color: #6b7280;
+      white-space: nowrap;
+    }
+
+    .el-step__description {
+      margin-top: 10px;
+      font-size: 14px;
+      color: #333;
+      line-height: 22px;
+
+      .order-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        .order-node {
+          font-size: 16px;
+          color: #333;
+          line-height: 22px;
+        }
+
+        .order-desc {
+          max-width: 250px;
+          font-size: 14px;
+          color: #999;
+          line-height: 22px;
+        }
+      }
+    }
+  }
+
+
 }
 </style>
