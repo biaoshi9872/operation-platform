45dc0ee36f7755cdfdb2d4da89bb799240371f55
shibiao <1508113513@qq.com>
2026-01-08 15:13:24 +0800

    fix:应用新增加解密字段

--- src/views/application/components/ApplicationModel.vue
+++ src/views/application/components/ApplicationModel.vue
@@ -38,6 +38,7 @@ const data = reactive<IData>({
     vpOderCallBackUrl: '',
     vpOderVerificationCallBackUrl: '',
     developerEmail: '',
+    isEncrypt:1,
     developerPhone: ''
   },
   goodsTypeList: [],
@@ -323,8 +324,16 @@ const blurHandler = () => {
             trigger: ['blur', 'change']
           }
           ]">
-          <el-input v-model="data.formData.vpOderVerificationCallBackUrl" placeholder="请输入虚拟下单回调地址">
+          <el-input v-model="data.formData.vpOderVerificationCallBackUrl" placeholder="请输入虚拟订单核销回调地址">
           </el-input>
+        </el-form-item>
+        <el-form-item v-if="data.formData.goodsSourceTypeCodeList?.includes(106)" label="回调是否加解密" prop="isEncrypt">
+          <el-radio-group v-model="data.formData.isEncrypt">
+            <!-- works when >=2.6.0, recommended ✔️ not work when <2.6.0 ❌ -->
+            <el-radio :value="1">是</el-radio>
+            <!-- works when <2.6.0, deprecated act as value when >=3.0.0 -->
+            <el-radio :value="0">否</el-radio>
+          </el-radio-group>
         </el-form-item>
       </el-form>
     </div>
